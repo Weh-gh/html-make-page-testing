786db001147ab1ceb597978d555019f8b4671870
Weh <turkkingtrgaming@gmail.com>
2022-03-19 05:36:16 +0300

    tt

--- assets/ModalContent.js
+++ assets/ModalContent.js
@@ -1,31 +1,31 @@
-window.onload=function() {
-    console.log("Open")
-}
+// window.onload=function() {
+//     console.log("Open")
+// }
 
-// Get modal element
-var modal = document.getElementById('simpleModal');
+// // Get modal element
+// var modal = document.getElementById('simpleModal');
 
-// Get open modal button
-var modalBtn = document.getElementById('modalBtn');
+// // Get open modal button
+// var modalBtn = document.getElementById('modalBtn');
 
-// Get close button
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
+// // Get close button
+// var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-// Listen for click
-var modalBtn = document.getElementById("modalBtn");
+// // Listen for click
+// var modalBtn = document.getElementById("modalBtn");
 
-// Listen for open click
-modalBtn.addEventListener("click", openModal)
+// // Listen for open click
+// modalBtn.addEventListener("click", openModal)
 
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
+// // Listen for close click
+// closeBtn.addEventListener('click', closeModal);
 
-//function to open modal
-function openModal() {
-    modal.style.display = 'block';
-}
+// //function to open modal
+// function openModal() {
+//     modal.style.display = 'block';
+// }
 
-// Close
-function closeModal() {
-    modal.style.display = 'none';
-}
+// // Close
+// function closeModal() {
+//     modal.style.display = 'none';
+// }
